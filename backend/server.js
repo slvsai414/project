aeeac037b0b0f4dd277cc018299110b457befb7a
Venkatsai0105 <40845818+slvsai414@users.noreply.github.com>
2025-02-12 23:20:55 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,7 @@ app.use(express.json());
 dotenv.config();
 
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-    ? "https://cms-frontend-0rrx.onrender.com"
-    :"http://localhost:5173",
+  origin: "https://cms-frontend-0rrx.onrender.com"
 
   methods: ["GET", "POST", "PUT", "DELETE"],
 
