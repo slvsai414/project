1753228acb12df3ad8721e8bd4f92728620db024
Venkatsai0105 <40845818+slvsai414@users.noreply.github.com>
2025-02-12 14:19:43 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 dotenv.config();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["https://cms-yikc.onrender.com"]
 
   methods: ["GET", "POST", "PUT", "DELETE"],
 
