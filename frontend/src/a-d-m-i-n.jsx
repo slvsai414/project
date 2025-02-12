6525be5ca59f528de0071724d18a0c71ffd03a84
Venkatsai0105 <40845818+slvsai414@users.noreply.github.com>
2025-02-12 22:47:36 +0530

    a-d-m-i-n.jsx

--- frontend/src/a-d-m-i-n.jsx
+++ frontend/src/a-d-m-i-n.jsx
@@ -13,7 +13,7 @@ const UpdateStudent = () => {
         e.preventDefault();
         setLoading(true);
 
-            const adminData = await axios.get("https://cms-frontend-0rrx.onrender.com/",{withCredentials:true})
+            const adminData = await axios.get("https://cms-yikc.onrender.com/",{withCredentials:true})
             //console.log(adminData)
 
 
