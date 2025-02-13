0d7c52f64fe22e7cbe27cdea05ebd77ec7fc7349
Venkatsai0105 <40845818+slvsai414@users.noreply.github.com>
2025-02-13 21:19:48 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link, useLocation } from "react-router-dom";
 import Landing_Page from "./index1";
 import Dash from "./dashboard";
 import { Bell } from 'lucide-react';
