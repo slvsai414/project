2b7e4cd730367b4b455ba416489e37ce4becb54c
Venkatsai0105 <40845818+slvsai414@users.noreply.github.com>
2025-02-15 22:34:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -134,7 +134,7 @@ app.post('/login', async(req,res) =>{
       if (isMatch){
         const token = jwt.sign({email:checkUser.email},"we-go-jim",{expiresIn:"1d"});
         
-        res.cookie("token",token,{httpOnly:true, secure:true, sameSite:"None", expires: new Date(Date.now() + 24 * 60 * 60 * 1000)})
+        res.cookie("token",token,{httpOnly:true, secure:true, sameSite:"Lax", expires: new Date(Date.now() + 24 * 60 * 60 * 1000)})
         res.status(200).json({ status: "Success", message: `Welcome, ${checkUser.name}!` });
       }else{
         res.json("Wrong Password")
